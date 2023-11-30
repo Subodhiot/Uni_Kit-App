b662ee55e6e963cae89e63dab9878f3b6c7d9831
Subodh <sharmasubodh744@gmail.com>
2023-11-30 08:08:26 +0530

    change bg

--- app/src/main/java/com/falcon/unikit/TextWithBorder.kt
+++ app/src/main/java/com/falcon/unikit/TextWithBorder.kt
@@ -4,6 +4,7 @@ package com.falcon.unikit
 import android.content.ClipDescription
 import android.content.Context
 import android.util.Log
+import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
@@ -25,6 +26,7 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.geometry.Size
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.onGloballyPositioned
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalDensity
@@ -39,6 +41,7 @@ fun TextWithBorder(headingValue: String, descriptionValue: String){
     var mTextFieldSize by remember { mutableStateOf(Size.Zero)}
     Column(
         Modifier
+            .background(color = Color.Gray)
             .padding(10.dp, 5.dp)
     ) {
         OutlinedTextField(
